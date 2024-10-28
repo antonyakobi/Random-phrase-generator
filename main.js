423d4d2c46b894f82bc408d832927fb4af088ec5
antonyakobi <antonyakobi@yandex.by>
2024-10-29 00:13:26 +0300

    Refactoring2

--- main.js
+++ main.js
@@ -3,22 +3,16 @@ function getRandomElement(array) {
     return array[randomIndex]
 }
 
-function makePhrases() {
-    const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
-    const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
-    const words3 = ["морковку", "макороны", "косточку"];
+const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
+const words2 = ["любит есть", "хочет погрызть", "всегда ищет"];
+const words3 = ["морковку", "макороны", "косточку"];
 
-    // const randomIndex_1 = Math.floor(Math.random() * words1.length)
-    // const randomIndex_2 = Math.floor(Math.random() * words1.length)
-    // const randomIndex_3 = Math.floor(Math.random() * words1.length)
-    // const phrase = words1[randomIndex_1] + " " + words2[randomIndex_2] + " " + words3[randomIndex_3]
-
-    // const phrase = getRandomElement(words1) + " " + getRandomElement(words2) + " " + getRandomElement(words3)
-    // return phrase
-
-    return getRandomElement(words1) + " " + getRandomElement(words2) + " " + getRandomElement(words3)
+function makePhrases(param1, param2, param3) {
+    
+    const result = getRandomElement(param1) + " " + getRandomElement(param2) + " " + getRandomElement(param3)
+    return result
 }
 
-alert(makePhrases())
-console.log(makePhrases());
+alert(makePhrases(words1, words2, words3))
+console.log(makePhrases(words1, words2, words3));
 
